297d7b3c337278af2d4c7e659fbe8cdfd7e8f3ef
mspruc <55182629+mspruc@users.noreply.github.com>
2021-11-06 15:31:22 +0100

    Initial commit

---
+++ src/Entity.java
@@ -0,0 +1,14 @@
+public class Entity{
+
+    int posX, posY;
+
+    Entity() {
+
+    }
+
+
+    Entity(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
---
+++ src/Tile.java
@@ -0,0 +1,10 @@
+public class Tile {
+    /*
+    Each tile is a square on the 2d map
+
+     */
+
+    public Tile(int x, int y) {
+
+    }
+}
